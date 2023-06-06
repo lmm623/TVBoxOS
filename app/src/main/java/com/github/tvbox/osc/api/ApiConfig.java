de47e234537e8f532c6d34a7eee82d112fec3279
lmm623 <126368753+lmm623@users.noreply.github.com>
2023-06-06 20:31:30 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://am.adianshi.com:9805/.aming/TVBOX/tvbox.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
